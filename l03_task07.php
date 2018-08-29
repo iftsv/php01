9b9e067337a8f4a397f22c06e9775c3b37d35623
ifetisov <i.fetisoff@gmail.com>
2018-08-29 09:28:14 +0300

    Домашнее задание Урок 3

---
+++ l03_task07.php
@@ -0,0 +1,21 @@
+<head>
+	<meta charset="UTF-8">
+</head>
+<body>
+	<?
+	echo "<b>Домашнее задание Урок 3 Задача 7</b><br>";
+	echo "Вывести с помощью цикла for числа от 0 до 9, НЕ используя тело цикла.<br>";
+	echo "<br><b>Решение:</b><br>";
+	
+
+	function forwithoutBody() {
+		for($i = 0; $i <= 9; print $i++) {
+			# empty
+		}
+	}
+	forwithoutBody();
+
+	?>
+
+
+</body>
