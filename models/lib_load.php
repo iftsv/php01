968f367f80263ff0edf3ece3bc548dceb7264ffb
elias_laptop <i.fetisoff@gmail.com>
2018-09-20 10:48:55 +0300

    Для проверки можно использовать этих пользователей пользователь admin@lp.test пароль admin пользователь user@lp.test пароль 123
    Что сделано:
    +админка сайта - редактирование, добавление товара в архив, добавление нового товара
    +каталог товаров и страница с характеристиками товаров
    +регистрация нового пользователя
    +интерфейс разработан под две группы пользователей - admin и customer.
    Группа admin имеет право доступа к админке сайта/доступ ко всем заказам и изменение статуса заказа. Customer только для работы с корзиной и заказами
    
    Что нужно доделать:
    -Необходимо добавить работу с корзиной
    -Необходимо добавить работу с заказами
    
    Из-за отпуска не успеваю реализовать последние два пункта в срок.
    Постараюсь реализовать до конца срока оставишийся функционал.

---
+++ models/lib_load.php
@@ -0,0 +1,10 @@
+<?
+/*
+загрузка необходимых файлов
+*/
+include_once (LIB_DIR . "/sqlconnect.lib.php");
+include_once (LIB_DIR . "/auth.lib.php");
+include_once (LIB_DIR . "/mainfunc.lib.php");
+include_once (LIB_DIR . "/feedbackfunc.lib.php");
+
+
